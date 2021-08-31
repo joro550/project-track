cb020a6996815daac61a1308b6bbaa6268667fa2
joro550 <markdavies550@gmail.com>
2021-08-31 14:45:12 +0100

    Adding server auth

--- Server/Controllers/WeatherForecastController.cs
+++ Server/Controllers/WeatherForecastController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Project.Track.Shared;
 
 namespace Project.Track.Server.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
