72eab579f2cb2a68afd48dfc1cbe9b91a1fa4681
Mark <markdavies550@gmail.com>
2021-09-01 19:45:56 +0100

    Fixing auth stuff

--- Client/Program.cs
+++ Client/Program.cs
@@ -24,9 +24,9 @@ namespace Project.Track.Client
 
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
                 .CreateClient("ServerAPI"));
-
-            builder.Services.AddScoped(sp => new HttpClient
-                { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //
+            // builder.Services.AddScoped(sp => new HttpClient
+            //     { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddOidcAuthentication(options =>
             {
